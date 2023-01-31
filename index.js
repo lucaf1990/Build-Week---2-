a484abb035dabd5bde8db36d054373eee83670ea
lucaf1990 <117093377+lucaf1990@users.noreply.github.com>
2023-01-31 22:20:37 +0100

    f

--- index.js
+++ index.js
@@ -10,17 +10,22 @@ const secondSection = async () => {
   let songs = await fetchByQuery("dance");
   console.log(songs);
   let row = document.querySelector(".secondSection");
-  let mySongs = [songs[1], songs[2], songs[6], songs[8], songs[15], songs[20]];
+  let mySongs = [songs[4], songs[2], songs[10], songs[8], songs[15], songs[20]];
   console.log(mySongs);
   mySongs.forEach(({ album, title, link }) => {
-    row.innerHTML += `<div class="card">
+    row.innerHTML += `<div class="card" style="width: 252px">
+    <div>
     <img
       src="${album.cover_small}"
       alt="${album.title}"
       height="80px"
     />
-    <div>
-    <h5>${title}</h5>
+    </div>
+    <div class="title" style="width:100%; justify-content:left">
+    <p>${title}</p>
+    </div>
+    <div class="play-icon">
+    <ion-icon name="play-circle-outline" class="main-play-icon"></ion-icon>
     </div>
   </div>`;
   });
