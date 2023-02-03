14ea87932fe5aa2f7466edc06a55dc3c4dd4d06a
Alberto Macis <bebo.macis@gmail.com>
2023-02-03 08:46:57 +0100

    add

--- search.js
+++ search.js
@@ -21,7 +21,7 @@ let myFunc = async () => {
       <div class="card m-3 bg-dark" style="width: 18rem;">
         <img class="mt-3" src="${songs[i].album.cover_medium}" class="card-img-top" alt="${songs[i].title}">
         <div class="card-body">
-        <a href="./album.html"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html"><p class="card-text">${songs[i].artist.name}</p></a>
+        <a href="./album.html?idAlbum=${songs[i].album.id}"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html?idArtist=${songs[i].artist.id}"><p class="card-text">${songs[i].artist.name}</p></a>
     
         </div>
       </div>
@@ -31,7 +31,7 @@ let myFunc = async () => {
       <div class="card m-3 bg-dark" style="width: 18rem;">
         <img class="mt-3" src="${songs[i].album.cover_medium}" class="card-img-top" alt="${songs[i].title}">
         <div class="card-body">
-        <a href="./album.html"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html"><p class="card-text">${songs[i].artist.name}</p></a>
+        <a href="./album.html?idAlbum=${songs[i].album.id}"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html?idArtist=${songs[i].artist.id}"><p class="card-text">${songs[i].artist.name}</p></a>
     
         </div>
       </div>
@@ -41,7 +41,7 @@ let myFunc = async () => {
       <div class="card m-3 bg-dark" style="width: 18rem;">
         <img class="mt-3" src="${songs[i].album.cover_medium}" class="card-img-top" alt="${songs[i].title}">
         <div class="card-body">
-        <a href="./album.html"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html"><p class="card-text">${songs[i].artist.name}</p></a>
+        <a href="./album.html?idAlbum=${songs[i].album.id}"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html?idArtist=${songs[i].artist.id}"><p class="card-text">${songs[i].artist.name}</p></a>
     
         </div>
       </div>
@@ -51,7 +51,7 @@ let myFunc = async () => {
       <div class="card m-3 bg-dark" style="width: 18rem;">
         <img class="mt-3" src="${songs[i].album.cover_medium}" class="card-img-top" alt="${songs[i].title}">
         <div class="card-body">
-        <a href="./album.html"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html"><p class="card-text">${songs[i].artist.name}</p></a>
+        <a href="./album.html?idAlbum=${songs[i].album.id}"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html?idArtist=${songs[i].artist.id}"><p class="card-text">${songs[i].artist.name}</p></a>
     
         </div>
       </div>
@@ -61,7 +61,7 @@ let myFunc = async () => {
       <div class="card m-3 bg-dark" style="width: 18rem;">
         <img class="mt-3" src="${songs[i].album.cover_medium}" class="card-img-top" alt="${songs[i].title}">
         <div class="card-body">
-        <a href="./album.html"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html"><p class="card-text">${songs[i].artist.name}</p></a>
+        <a href="./album.html?idAlbum=${songs[i].album.id}"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html?idArtist=${songs[i].artist.id}"><p class="card-text">${songs[i].artist.name}</p></a>
     
         </div>
       </div>
@@ -71,7 +71,7 @@ let myFunc = async () => {
       <div class="card m-3 bg-dark" style="width: 18rem;">
         <img class="mt-3" src="${songs[i].album.cover_medium}" class="card-img-top" alt="${songs[i].title}">
         <div class="card-body">
-        <a href="./album.html"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html"><p class="card-text">${songs[i].artist.name}</p></a>
+        <a href="./album.html?idAlbum=${songs[i].album.id}"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html?idArtist=${songs[i].artist.id}"><p class="card-text">${songs[i].artist.name}</p></a>
     
         </div>
       </div>
@@ -81,7 +81,7 @@ let myFunc = async () => {
       <div class="card m-3 bg-dark" style="width: 18rem;">
         <img class="mt-3" src="${songs[i].album.cover_medium}" class="card-img-top" alt="${songs[i].title}">
         <div class="card-body">
-        <a href="./album.html"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html"><p class="card-text">${songs[i].artist.name}</p></a>
+        <a href="./album.html?idAlbum=${songs[i].album.id}"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html?idArtist=${songs[i].artist.id}"><p class="card-text">${songs[i].artist.name}</p></a>
     
         </div>
       </div>
@@ -91,7 +91,7 @@ let myFunc = async () => {
       <div class="card m-3 bg-dark" style="width: 18rem;">
         <img class="mt-3" src="${songs[i].album.cover_medium}" class="card-img-top" alt="${songs[i].title}">
         <div class="card-body">
-        <a href="./album.html"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html"><p class="card-text">${songs[i].artist.name}</p></a>
+        <a href="./album.html?idAlbum=${songs[i].album.id}"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html?idArtist=${songs[i].artist.id}"><p class="card-text">${songs[i].artist.name}</p></a>
     
         </div>
       </div>
@@ -101,7 +101,7 @@ let myFunc = async () => {
       <div class="card m-3 bg-dark" style="width: 18rem;">
         <img class="mt-3" src="${songs[i].album.cover_medium}" class="card-img-top" alt="${songs[i].title}">
         <div class="card-body">
-        <a href="./album.html"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html"><p class="card-text">${songs[i].artist.name}</p></a>
+        <a href="./album.html?idAlbum=${songs[i].album.id}"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html?idArtist=${songs[i].artist.id}"><p class="card-text">${songs[i].artist.name}</p></a>
     
         </div>
       </div>
@@ -113,7 +113,7 @@ let myFunc = async () => {
       <div class="card m-3 bg-dark" style="width: 18rem;">
         <img class="mt-3" src="${songs[i].album.cover_medium}" class="card-img-top" alt="${songs[i].title}">
         <div class="card-body">
-        <a href="./album.html"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html"><p class="card-text">${songs[i].artist.name}</p></a>
+        <a href="./album.html?idAlbum=${songs[i].album.id}"><h5 class="card-title">${songs[i].title}</h5></a>  <a href="./artists.html?idArtist=${songs[i].artist.id}"><p class="card-text">${songs[i].artist.name}</p></a>
     
         </div>
       </div>
